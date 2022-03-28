2aee6d2d225c0a2a1e1078d8fb57fe7ab5a853da
VWaghray <87341415+VWaghray@users.noreply.github.com>
2022-03-28 17:43:36 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,12 @@
-noseX = 0;
-noseY = 0;
 difference = 0;
 rightWristX = 0;
 leftWristX = 0;
 
-
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
-
     canvas = createCanvas(550, 550);
     canvas.position(560, 150);
-
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
 }
@@ -23,23 +18,17 @@ function modelLoaded() {
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
-        noseX = results[0].pose.nose.x;
-        noseY = results[0].pose.nose.y;
-        console.log("noseX = " + noseX + " noseY = " + noseY);
-
         leftWristX = results[0].pose.leftWrist.x;
         rightWristX = results[0].pose.rightWrist.x;
         difference = floor(leftWristX - rightWristX);
-
-        console.log("leftWristX = " + leftWristX + "rightWristX = " + rightWristX + " difference = " + difference);
+        console.log("leftWristX = " + leftWristX + " rightWristX = " + rightWristX + " difference = " + difference);
     }
 }
 
 function draw() {
-    background('#969A97');
-
+    background('#6C91C2');
     document.getElementById("font_size").innerHTML = "Font size of the text will be = " + difference + "px";
     textSize(difference);
-    fill('#F90093');
-    text('Vihaan', 5, 400);
+    fill('#FFE787');
+    text('Vihaan', 50, 400);
 }
