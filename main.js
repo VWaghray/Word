e37969b011a847f8d6607db63d52e69394b743b2
VWaghray <87341415+VWaghray@users.noreply.github.com>
2022-03-28 16:43:33 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,32 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightWrist = 0;
+leftWrist = 0;
+
+
+function setup(){
+video =  createCapture(VIDEO);
+  video.size(550, 500);
+
+  canvas = createCanvas(550, 550);
+  canvas.position(560,150);
+  
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+console.log('PoseNet Is Initialized!');
+}
+
+function gotPoses(results){
+if(results.length > 0)
+{
+console.log(results);
+}
+}
+
+function draw(){
+  background('#969A97');
+}
